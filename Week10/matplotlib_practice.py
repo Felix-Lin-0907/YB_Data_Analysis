3e8c96ffaa2458cf1120d8539e882f98cac6cac0
Yongfu Lin <felixlin0907@gmail.com>
2025-02-13 13:16:39 +1300

    barchart

--- Week10/matplotlib_practice.py
+++ Week10/matplotlib_practice.py
@@ -1,9 +1,10 @@
+from turtle import color
 import matplotlib.pyplot as plt
 import os
 
 project_dir = os.path.dirname(os.path.abspath(__file__))  # 适用于 Python 脚本
 save_dir = os.path.join(project_dir, "Output")
-save_path = os.path.join(save_dir, "sin_function.png")
+os.makedirs(save_dir, exist_ok=True)
 
 def basicPractice():
     x = [1,2,3,4,5]
@@ -14,11 +15,22 @@ def basicPractice():
     plt.ylabel("Y-Axis")
     plt.title("Basic Line Plot")
 
-    os.makedirs(save_dir, exist_ok=True)
+    save_path = os.path.join(save_dir, "basicPractice.png")
     plt.savefig(save_path, dpi=300, bbox_inches='tight')
-
     plt.show()
 
+def barChart():
+    categories = ["A", "B", "C", "D"]
+    values = [3, 7, 1, 8]
+    plt.bar(categories, values, color="skyblue")
+    plt.xlabel("Categories")
+    plt.ylabel("Values")
+    plt.title("Bar Chart Example")
+
+    save_path = os.path.join(save_dir, "barChart.png")
+    plt.savefig(save_path, dpi=300, bbox_inches='tight')
+    plt.show()
 
 if __name__ == "__main__":
-    basicPractice()
+    # basicPractice()
+    barChart()
